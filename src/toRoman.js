3a7162d7a06c0b03027c72e79c22f1500b0236a1
Dorota Pindur <dpindur@wp.pl>
2021-02-12 15:28:12 +0100

    test singles and tens in toRoman function

--- src/toRoman.js
+++ src/toRoman.js
@@ -1,11 +1,70 @@
 function toRoman(arabic) {
-    let roman = 'none';
-    if (arabic == 1) {
-        roman = 'I'
+    let roman;
+    let romanSingle = '';
+    let romanTens = '';
+
+    if (arabic <= 0) {
+        roman = 'none';
+    } else {
+   
+        if (arabic == 1) {
+            romanSingle = 'I'
+        }
+        if (arabic == 2) {
+            romanSingle = 'II'
+        }
+        if (arabic == 3) {
+            romanSingle = 'III'
+        }
+        if (arabic == 4) {
+            romanSingle = 'IV'
+        }
+        if (arabic == 5) {
+            romanSingle = 'V'
+        }
+        if (arabic == 6) {
+            romanSingle = 'VI'
+        }
+        if (arabic == 7) {
+            romanSingle = 'VII'
+        }
+        if (arabic == 8) {
+            romanSingle = 'VIII'
+        }
+        if (arabic == 9) {
+            romanSingle = 'IX'
+        }
+
+        if (arabic == 10) {
+            romanTens = 'X'
+        }
+        if (arabic == 20) {
+            romanTens = 'XX'
+        }
+        if (arabic == 30) {
+            romanTens = 'XXX'
+        }
+        if (arabic == 40) {
+            romanTens = 'XL'
+        }
+        if (arabic == 50) {
+            romanTens = 'L'
+        }
+        if (arabic == 60) {
+            romanTens = 'LX'
+        }
+        if (arabic == 70) {
+            romanTens = 'LXX'
+        }
+        if (arabic == 80) {
+            romanTens = 'LXXX'
+        }
+        if (arabic == 90) {
+            romanTens = 'XC'
+        }
+        roman = romanTens + romanSingle;
     }
-    if (arabic == 5) {
-        roman = 'V'
-    }
+
     return roman;
 }
 
--- src/toRoman.test.js
+++ src/toRoman.test.js
@@ -3,7 +3,28 @@ import toRoman from './toRoman';
 describe('toRoman()', () => {
     it.each([
         [1, 'I'],
+        [2, 'II'],
+        [3, 'III'],
+        [4, 'IV'],
         [5, 'V'],
+        [6, 'VI'],
+        [7, 'VII'],
+        [8, 'VIII'],
+        [9, 'IX']
+    ])('converts %d to %s', (arabic, expectedRoman) => {
+        expect(toRoman(arabic)).toEqual(expectedRoman)
+    });
+
+    it.each([
+        [10, 'X'],
+        [20, 'XX'],
+        [30, 'XXX'],
+        [40, 'XL'],
+        [50, 'L'],
+        [60, 'LX'],
+        [70, 'LXX'],
+        [80, 'LXXX'],
+        [90, 'XC']
     ])('converts %d to %s', (arabic, expectedRoman) => {
         expect(toRoman(arabic)).toEqual(expectedRoman)
     });
